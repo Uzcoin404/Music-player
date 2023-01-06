957a1e87878f9180dc18c1537f05993bb09cb4d5
Your Name <uzcointg@gmail.com>
2023-01-06 20:13:43 +0500

    some changes

--- js/music_list.js
+++ js/music_list.js
@@ -4,34 +4,34 @@ let musicList = [
         artist: "TheFatRat",
         img: "img/Rise_up.png",
         src: "audio/TheFatRat-Rise_Up.mp3",
-        love: false
+        love: false,
     },
     {
         name: "Fairytale",
         artist: "Alexander Rybak",
         img: "img/fairytale.jpg",
         src: "audio/Alexander_Rybak-Fairytale.mp3",
-        love: false
+        love: false,
     },
     {
         name: "Impossible",
         artist: "James Arthur",
         img: "img/impossible.jpg",
         src: "audio/James_Arthur-Impossible.mp3",
-        love: false
+        love: false,
     },
     {
         name: "Landscape",
         artist: "Jarico",
         img: "img/landscape.jpg",
         src: "audio/Jarico-Landscape.mp3",
-        love: false
+        love: false,
     },
     {
         name: "Diamonds",
         artist: "Rihanna",
         img: "img/diamonds.jpg",
         src: "audio/Rihanna-diamonds.mp3",
-        love: false
-    }
-]
+        love: false,
+    },
+];
